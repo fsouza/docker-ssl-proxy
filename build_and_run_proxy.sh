3357b1a9b42e53b3b20ad5750e7409c58cab4aa7
Christopher Bachicha <cbachich@gmail.com>
2015-04-03 14:31:07 -0600

    Add params for docker name and port

--- build_and_run_proxy.sh
+++ build_and_run_proxy.sh
@@ -1,11 +1,49 @@
 #!/bin/sh
 
 appname="bixel"
+port=443
+
+while getopts ':d::p:' opt; do
+  case "$opt" in
+    d)
+      appname=$OPTARG
+      ;;
+    p)
+      port=$OPTARG
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+    *)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+
 proxyname="${appname}_proxy"
 
+echo === Environment ===
+echo Bixel App: ${appname}
+echo Proxy App Name: ${proxyname}
+echo SSL Port: ${port}
+
+echo
+echo == Creating Nginx Config ==
+cat parts/nginx_pre.conf > nginx.conf
+echo "        listen ${port};" >> nginx.conf
+cat parts/nginx_post.conf >> nginx.conf
+
+echo
 echo === Building Proxy Image... ===
 docker rm -f ${proxyname}
 docker build -t ${proxyname} .
 
+echo
 echo === Running Bixel Image... ===
-docker run -d -p 443:443 --name="${proxyname}" --link ${appname}:nodeapp  ${proxyname}
+docker run -d -p ${port}:${port} \
+--name="${proxyname}" \
+--link ${appname}:proxyapp  \
+--env SSL_PORT=${port} \
+${proxyname}
