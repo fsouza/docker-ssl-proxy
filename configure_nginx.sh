998c1df6d3a4ce24214d9d59dc03783be96ef935
Francisco Souza <francisco.souza@nytimes.com>
2016-06-16 14:10:16 -0300

    Add a hack to prevent breaking nginx.conf

--- configure_nginx.sh
+++ configure_nginx.sh
@@ -4,6 +4,9 @@ export PORT=${PORT:-443}
 export TARGET_PORT=${TARGET_PORT:-80}
 export TARGET_HOST=${TARGET_HOST:-proxyapp}
 
+# Hack to avoid breaking nginx.conf
+export host='$host' remote_addr='$remote_addr' proxy_add_x_forwarded_for='$proxy_add_x_forwarded_for' scheme='$scheme' remote_addr='$remote_addr'
+
 echo "Starting Proxy: $PORT"
 echo "Target Docker Port: $TARGET_PORT"
 
